ecb297bfa61caad52a571555f6c00bb096420202
Marcin Urban <marcinurban560@gmail.com>
2022-08-18 16:50:56 +0200

    Starter

---
+++ app.js
@@ -0,0 +1,15 @@
+const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+
+const HEAL_VALUE = 20;
+
+const MONSTER_ATTACK_VALUE = 14;
+const MONSTER_STRONG_ATTACK_VALUE = 24;
+
+let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+attackBtn.addEventListener('click', attackHandler);
