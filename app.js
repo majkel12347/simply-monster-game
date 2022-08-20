2a368ccf89be1a168a02e6557bdc33fa6b9d92f9
Marcin Urban <marcinurban560@gmail.com>
2022-08-20 23:31:55 +0200

    1a

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
 const ATTACK_VALUE = 10;
 const ATTACK_MONSTER_VALUE = 17;
 
+const STRONG_ATTACK_VALUE = 24;
+
 const chosenMaxLife = 100;
 const attackValue = 10;
 
 let currentPlayerHealth = chosenMaxLife;
 let currentMonsterHealth = chosenMaxLife;
 
-//1 adding attack function
-
-function attackHandler() {
-  let damage = dealMonsterDamage(ATTACK_VALUE);
-  currentPlayerHealth -= damage;
+// adding strong attack function
+function strongAttack() {
+  let damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+  currentMonsterHealth -= damage;
 
   let monsterDamage = dealPlayerDamage(ATTACK_MONSTER_VALUE);
-  currentMonsterHealth -= monsterDamage;
+  currentPlayerHealth -= monsterDamage;
+
+  if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    console.log('You lose!!!');
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    console.log('You win!!!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    console.log('Draw!!!');
+  }
+}
+
+//1 adding attack function
+function attackHandler() {
+  // set damage
+  let damage = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHealth -= damage;
+
+  let monsterDamage = dealPlayerDamage(ATTACK_MONSTER_VALUE);
+  currentPlayerHealth -= monsterDamage;
+
+  if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    console.log('You lose!!!');
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    console.log('You win!!!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    console.log('Draw!!!');
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttack);
